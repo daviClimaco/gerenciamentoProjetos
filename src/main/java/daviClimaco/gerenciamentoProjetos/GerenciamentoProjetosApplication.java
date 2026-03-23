50a73359e787690b91b2018a2fd4f95a4d9f06f4
daviClimaco <davibomfimclimaco@gmail.com>
2026-03-23 18:42:20 -0300

    feat: criação do repositório git
    Co-authored-by: heitor couto <heitor.ti.couto@gmail.com>
    Co-authored-by: talis <talissossai9@gmail.com>

---
+++ src/main/java/daviClimaco/gerenciamentoProjetos/GerenciamentoProjetosApplication.java
@@ -0,0 +1,13 @@
+package daviClimaco.gerenciamentoProjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daviClimaco/gerenciamentoProjetos/GerenciamentoProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package daviClimaco.gerenciamentoProjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
